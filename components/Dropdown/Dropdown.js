8bfa14d7ee137723bc032b9e27ba917aa656db50
Andy Bettisworth <andy@accreu.com>
2019-04-11 15:07:48 -0500

    Setup the Tabs component

--- components/Dropdown/Dropdown.js
+++ components/Dropdown/Dropdown.js
@@ -17,9 +17,7 @@ class Dropdown {
     this.content = this.element.querySelector(".dropdown-content")
 
     // Add a click handler to the button reference and call the toggleContent method.
-    this.button.addEventListener('click', () => {
-      this.toggleContent()
-    })
+    this.button.addEventListener('click', () => this.toggleContent())
   }
 
   toggleContent() {
